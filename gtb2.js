a4b399755f785f9fe45146a21b4094b62168a393
Deejay000 <destinyjeffrey203@gmail.com>
2024-06-14 20:27:28 +0100

    Update gtb2.js

--- gtb2.js
+++ gtb2.js
@@ -43,67 +43,27 @@ document.getElementById('seven-info').textContent = sevenInfo;
 
 
 
-months = ["01","02","03","04",'05',"06","07","08","09","10","11","12"]
-
-//days = ["05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","01","02","03","04"]
-
-date_data = new Date()
-
-current_date = date_data.getDate()
-
-
-document.getElementById('date').textContent = `${current_date}`
-
-current_month = date_data.getMonth()
-
-document.getElementById('month').textContent = `${months[current_month]}`
-
-current_year = date_data.getFullYear()
-
-document.getElementById('year').textContent = `${current_year}`
-
-
-current_hour = date_data.getHours()
-current_minute = date_data.getMinutes()
-
-am_pm = ''
-
-if(current_hour >= 12){
-  am_pm = 'PM'
-}else {
-  am_pm = 'AM'
-}
-
-document.getElementById('time').textContent = `${current_hour}:${current_minute} ${am_pm}`
-
-
 //SECOND CODE
 
-monthss = ['01','02','03','04','05','06','07','08','09','10','11','12'];
 
-
-monthss = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
+months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
 
  date_data = new Date();
 
 current_day = date_data.getDay();
 
 
-document.getElementById('dayy').textContent = `${dayss[current_day]}`;
+document.getElementById('day').textContent = `${dayss[current_day]}`;
 
-current_date = date_data.getDate()
-
-
-document.getElementById('datee').textContent = `${current_date}`
 
  current_date = date_data.getDate();
-document.getElementById('datee').textContent = `${current_date}`;
+document.getElementById('date').textContent = `${current_date}`;
 
 current_month = date_data.getMonth();
-document.getElementById('monthh').textContent = `${months[current_month]}`;
+document.getElementById('month').textContent = `${months[current_month]}`;
 
  current_year = date_data.getFullYear();
-document.getElementById('yearr').textContent = `${current_year}`;
+document.getElementById('year').textContent = `${current_year}`;
 
 current_hour = date_data.getHours();
  current_minute = date_data.getMinutes();
@@ -125,7 +85,7 @@ if (current_hour === 0) {
 // Ensure minutes are two digits
 current_minute = current_minute < 10 ? '0' + current_minute : current_minute;
 
-document.getElementById('timee').textContent = `${current_hour}:${current_minute} ${am_pm}`;
+document.getElementById('time').textContent = `${current_hour}:${current_minute} ${am_pm}`;
 
 
 
