79ac3ede9f46d7371bc8c647792d9506e9b095ed
Deejay000 <destinyjeffrey203@gmail.com>
2024-06-17 21:44:48 +0100

    Update paypal.js

--- paypal.js
+++ paypal.js
@@ -7,3 +7,10 @@ const firstInfo = localStorage.getItem('first-info');
   document.getElementById('first-info').textContent = firstInfo;
   document.getElementById('second-info').textContent = secondInfo;
   
+
+const amounts = document.querySelectorAll('.amount');
+amounts.forEach((num) => {
+  const formattedNum = num.textContent.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  num.textContent = formattedNum;
+});
+
