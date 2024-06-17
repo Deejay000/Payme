24484fce301ca374df4d0e50919762be3a798ffa
Deejay000 <destinyjeffrey203@gmail.com>
2024-06-17 22:15:06 +0100

    Update cashapp.js

--- cashapp.js
+++ cashapp.js
@@ -8,4 +8,9 @@ const firstInfo = localStorage.getItem('first-info');
   document.getElementById('second-info').textContent = secondInfo;
   
 
+const amounts = document.querySelectorAll('.amount');
+amounts.forEach((num) => {
+  const formattedNum = num.textContent.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  num.textContent = formattedNum;
+});
 
