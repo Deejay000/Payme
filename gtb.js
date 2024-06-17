490dc7155e8ed76515e4d98b2928a4c9d26b0908
Deejay000 <destinyjeffrey203@gmail.com>
2024-06-17 21:53:29 +0100

    Update gtb.js

--- gtb.js
+++ gtb.js
@@ -19,6 +19,11 @@ const firstInfo = localStorage.getItem('first-info');
   document.getElementById('eight-info').textContent = eightInfo;
 
 
+const amounts = document.querySelectorAll('.amount');
+amounts.forEach((num) => {
+  const formattedNum = num.textContent.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  num.textContent = formattedNum;
+});
 
 
 
