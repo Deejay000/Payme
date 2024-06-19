1a4c6f4e2a04a7181e81ffe3bcdc88e43de26fac
Deejay000 <destinyjeffrey203@gmail.com>
2024-06-19 12:41:09 +0100

    Update gtb2.js

--- gtb2.js
+++ gtb2.js
@@ -19,20 +19,24 @@ const firstInfo = localStorage.getItem('first-info');
   const fiveInfo = localStorage.getItem('five-info');
   const sixInfo = localStorage.getItem('six-info');
   const sevenInfo = localStorage.getItem('seven-info');
-  const eightInfo = localStorage.getItem('eight-info');
-  const nineInfo = localStorage.getItem('nine-info');
+  /*const eightInfo = localStorage.getItem('eight-info');
+  const nineInfo = localStorage.getItem('nine-info');*/
  
  
   
   document.getElementById('first-info').textContent = firstInfo;
   document.getElementById('second-info').textContent = secondInfo;
   document.getElementById('third-info').textContent = thirdInfo;
-  document.getElementById('four-info').textContent = fourInfo;
+    document.querySelectorAll('#four-info').forEach(element => {
+    element.textContent = fourInfo;
+});
   document.getElementById('five-info').textContent = fiveInfo;
-  document.getElementById('six-info').textContent = sixInfo;
+    document.querySelectorAll('#six-info').forEach(element => {
+    element.textContent = sixInfo;
+});
 document.getElementById('seven-info').textContent = sevenInfo;
-  document.getElementById('eight-info').textContent = eightInfo;
-  document.getElementById('nine-info').textContent = nineInfo;
+  /*document.getElementById('eight-info').textContent = eightInfo;
+  document.getElementById('nine-info').textContent = nineInfo;*/
 
 
 
