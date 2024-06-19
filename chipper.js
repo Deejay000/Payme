95021b7d96f36def85a41c1c154e9b2961e3fb13
Deejay000 <destinyjeffrey203@gmail.com>
2024-06-19 12:24:28 +0100

    Update chipper.js

--- chipper.js
+++ chipper.js
@@ -17,21 +17,26 @@ const firstInfo = localStorage.getItem('first-info');
   const fiveInfo = localStorage.getItem('five-info');
   const sixInfo = localStorage.getItem('six-info');
  const sevenInfo = localStorage.getItem('seven-info');
- const eightInfo = localStorage.getItem('eight-info');
- const nineInfo = localStorage.getItem('nine-info');
+ /*const eightInfo = localStorage.getItem('eight-info');
+ const nineInfo = localStorage.getItem('nine-info');*/
  
  
   
-  document.getElementById('first-info').textContent = firstInfo;
-  document.getElementById('second-info').textContent = secondInfo;
+  document.querySelectorAll('#first-info').forEach(element => {
+    element.textContent = firstInfo;
+});
+
+document.querySelectorAll('#second-info').forEach(element => {
+    element.textContent = secondInfo;
+});
   document.getElementById('third-info').textContent = thirdInfo;
   document.getElementById('four-info').textContent = fourInfo;
   document.getElementById('five-info').textContent = fiveInfo;
   document.getElementById('six-info').textContent = sixInfo;
   document.getElementById('seven-info').textContent = sevenInfo;
-document.getElementById('eight-info').textContent = eightInfo;
+/*document.getElementById('eight-info').textContent = eightInfo;
   document.getElementById('nine-info').textContent = nineInfo;
-
+*/
 
 const amounts = document.querySelectorAll('.amount');
 amounts.forEach((num) => {
