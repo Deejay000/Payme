27cc4cfdd324d3eca4314483627fcca8f5c154c4
Deejay000 <destinyjeffrey203@gmail.com>
2024-06-19 12:34:09 +0100

    Update zelle2.js

--- zelle2.js
+++ zelle2.js
@@ -1,18 +1,19 @@
 const firstInfo = localStorage.getItem('first-info');
   const secondInfo = localStorage.getItem('second-info');
   const thirdInfo = localStorage.getItem('third-info');
-  const fourInfo = localStorage.getItem('four-info');
-  const fiveInfo = localStorage.getItem('five-info');
+  /*const fourInfo = localStorage.getItem('four-info');
+  const fiveInfo = localStorage.getItem('five-info');*/
   const sixInfo = localStorage.getItem('six-info');
   
  
  
   
-  document.getElementById('first-info').textContent = firstInfo;
-  document.getElementById('second-info').textContent = secondInfo;
+    document.querySelectorAll('#first-info').forEach(element => {
+    element.textContent = firstInfo;
+}); document.getElementById('second-info').textContent = secondInfo;
   document.getElementById('third-info').textContent = thirdInfo;
-  document.getElementById('four-info').textContent = fourInfo;
-  document.getElementById('five-info').textContent = fiveInfo;
+  /*document.getElementById('four-info').textContent = fourInfo;
+  document.getElementById('five-info').textContent = fiveInfo;*/
   document.getElementById('six-info').textContent = sixInfo;
 
 
